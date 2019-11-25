4618dbb5d30bf6f65ba90f686fe9d97df1c40f96
Arian Gagica <gagicaarian@gmail.com>
2019-11-25 11:07:22 +0100

    init

---
+++ routes/users.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+var chatHistory = [];
+var nicknames = [];
+
+router.get('/', function(req, res, next) {
+	res.send({ message: 'fhs chat-app api works' });
+});
+
+/* GET users listing. */
+router.get('/history', function(req, res, next) {
+	res.send(chatHistory);
+});
+router.post('/history', function(req, res, next) {
+	var date = new Date();
+
+	chatHistory.push({ message: req.body.message, nickname: req.body.nickname, date: date });
+	res.json({ message: 'History created!' });
+});
+module.exports = router;
